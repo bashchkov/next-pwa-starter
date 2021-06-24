1c45a62b258970acbf8a5de206367904fe4b3d7c
Mikhail <mikhail@bashchkovcom>
2021-06-24 08:34:27 +0400

    update

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,56 +1,91 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import {useTranslation} from 'next-i18next';
 import {FloatingLabel, Form, Button, Card, Spinner} from 'react-bootstrap';
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
-import {ChangeLocale} from "../../components/ChnageLocale";
-import LogoSvg from "../../components/LogoSvg";
-import {useState} from "react";
-import {HOME_PAGE} from "../../constants/urls";
+import {ChangeLocale} from '../../components/ChnageLocale';
+import LogoSvg from '../../components/LogoSvg';
+import {useState} from 'react';
+import {HOME_PAGE} from '../../constants/urls';
 
 
 const AuthPage = () => {
 
     const [loading, setLoading] = useState(false);
 
+    const [validated, setValidated] = useState(false);
+
     const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log(data)
-        setLoading(true)
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+            const data = new FormData(event.currentTarget);
+            console.log(data)
+            setLoading(true)
+        }
+
+        setValidated(true);
     }
 
     const {t} = useTranslation('auth')
 
     return (
-        <main className='d-flex flex-column justify-content-center align-items-center vh-100 wh-100'>
-
-
-            <Card style={{maxWidth: '420px'}} className='w-100 shadow'>
-                <Card.Body className='p-5'>
-                    <Link href={HOME_PAGE}>
-                        <a className='d-block mb-5 text-center'>
-                            <img src='/logo/bashchkov-technology-360x140.png' style={{width: 220}}/>
-                        </a>
-                    </Link>
-                    {/*<LogoSvg width={80} className='mb-4'/>*/}
-                    <Form onSubmit={handleSubmit}>
-                        {/*<img className='mb-4' src='../assets/brand/bootstrap-logo.svg' alt='' width='72' height='57'/>*/}
-                        <FloatingLabel label={t('email')} className='mb-4'>
-                            <Form.Control type='email' placeholder={t('email')} disabled={loading}/>
-                        </FloatingLabel>
-                        <FloatingLabel label={t('password')} className='mb-4'>
-                            <Form.Control type='password' placeholder={t('password')} disabled={loading}/>
-                        </FloatingLabel>
-                        <Button variant='primary' size='lg' className='w-100 mb-4 text-uppercase' type='submit' disabled={loading}>
-                            {loading ? (
-                                <Spinner as="span" animation="border" role="status" aria-hidden="true"/>
-                            ) : t('sign_in')}
-                        </Button>
-                        <p className='mt-5 mb-3 text-muted'>&copy; 2017–2021</p>
-                        <ChangeLocale/>
-                    </Form>
-                </Card.Body>
-            </Card>
+        <main className=''>
+            <div className='container col-xl-10 col-xxl-8 px-4 py-5'>
+                <div className='row align-items-center g-lg-5 py-5'>
+                    <div className='col-lg-7 text-center text-lg-start'>
+                        <h1 className='display-4 fw-bold lh-1 mb-3'>Vertically centered hero sign-up form</h1>
+                        <p className='col-lg-10 fs-4'>Below is an example form built entirely with Bootstrap’s form
+                            controls. Each required form group has a validation state that can be triggered by
+                            attempting to submit the form without completing it.</p>
+                    </div>
+                    <div className='col-md-10 mx-auto col-lg-5'>
+                        <Form
+                            noValidate
+                            validated={validated}
+                            onSubmit={handleSubmit}
+                            className='p-4 p-md-5 rounded-3 bg-light shadow'
+                        >
+                            <Link href={HOME_PAGE}>
+                                <a className='d-block mb-4 text-center'>
+                                    <Image
+                                        src='/logo/bashchkov-technology-360x140.png'
+                                        layout='fixed'
+                                        width={180}
+                                        height={70}
+                                    />
+                                </a>
+                            </Link>
+                            <FloatingLabel label={t('email')} className='mb-3'>
+                                <Form.Control
+                                    type='email'
+                                    placeholder={t('email')}
+                                    disabled={loading}
+                                    required
+                                />
+                            </FloatingLabel>
+                            <FloatingLabel label={t('password')} className='mb-4'>
+                                <Form.Control
+                                    type='password'
+                                    placeholder={t('password')}
+                                    disabled={loading}
+                                    required
+                                />
+                            </FloatingLabel>
+                            <Button variant='primary' size='lg' className='w-100' type='submit' disabled={loading}>
+                                {loading ? (
+                                    <Spinner as='span' animation='border' role='status' aria-hidden='true'/>
+                                ) : t('sign_in')}
+                            </Button>
+                            <hr className='my-4'/>
+                            <small className='d-block text-muted text-center'>
+                                &copy; 2021 <Link href={HOME_PAGE}><a>Bashchkov Technology LLC</a></Link>
+                            </small>
+                        </Form>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
